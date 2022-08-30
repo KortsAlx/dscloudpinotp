8e26b6593a5d42f1dfc98792ab4a10b6dc00a39e
Brandon Alexis Martinez Cortes <brandon.martinez@docsolutions.com>
2022-08-30 00:37:16 -0500

    Initial Commit

---
+++ dscloudpinotp/dscloudpinotp.h
@@ -0,0 +1,18 @@
+//
+//  dscloudpinotp.h
+//  dscloudpinotp
+//
+//  Created by Brandon Alexis Martinez Cortes on 30/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for dscloudpinotp.
+FOUNDATION_EXPORT double dscloudpinotpVersionNumber;
+
+//! Project version string for dscloudpinotp.
+FOUNDATION_EXPORT const unsigned char dscloudpinotpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dscloudpinotp/PublicHeader.h>
+
+
